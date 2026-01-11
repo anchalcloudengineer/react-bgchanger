6dcc5848137d92d63366423e3f8e331474a1624d
anchalwebdeveloper <anchalagrahari567@gmail.com>
2026-01-11 18:43:15 +0530

    dockerfile added

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/react-bgchanger/',  // 👈 This is important!
+  base: '/',  // 👈 This is important!
   plugins: [react()],
 })
